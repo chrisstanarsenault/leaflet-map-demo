e48308a5ace40c9d00f8ad3078cd7fe8585376bb
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-05-10 15:28:42 -0400

    adding ACCESS_TOKEN route for netlify setup for testing.  Will remove if does not work

--- scripts/index.js
+++ scripts/index.js
@@ -119,7 +119,7 @@ async function loadMap() {
 
     let myMap = L.map("mapid").setView([0, 0], 3);
     L.tileLayer(
-      `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hyaXNzdGFuYXJzZW5hdWx0IiwiYSI6ImNrOWY1dDV4NjA5bnczZW9iNXB4dmkzcHIifQ.03UOcpgGEfNQbYGxuVBkBQ`,
+      `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.ACCESS_TOKEN}`,
       {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
